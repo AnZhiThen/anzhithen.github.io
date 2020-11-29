0bbddf4f8a5f2bf5e1288f8c3d0800f27689d75b
Then An Zhi <anzhi.then@gmail.com>
2020-11-29 17:46:32 +0800

    Updates

--- precache-manifest.b02c3bf88ad68261c4d8549b9473ac68.js
+++ precache-manifest.b02c3bf88ad68261c4d8549b9473ac68.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d328feffbc0ce8afc7f844b6ae88c0d0",
+    "revision": "55db03e2a9aa3a9a8e128aae6b3b464b",
     "url": "/index.html"
   },
   {
-    "revision": "7b0bc55e0253b18a0f6f",
-    "url": "/static/css/main.77efebc2.chunk.css"
+    "revision": "dbafcddd8916b95b9aca",
+    "url": "/static/css/main.a26d742d.chunk.css"
   },
   {
     "revision": "7583dd6145532ae65652",
     "url": "/static/js/2.14c50d01.chunk.js"
   },
   {
-    "revision": "7b0bc55e0253b18a0f6f",
-    "url": "/static/js/main.b49bc3bb.chunk.js"
+    "revision": "dbafcddd8916b95b9aca",
+    "url": "/static/js/main.0f510b79.chunk.js"
   },
   {
     "revision": "6a6a75446a286ed70198",
